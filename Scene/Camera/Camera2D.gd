102ea68d3428acc6f0a6f267f04a2105a2dfe854
KaiserHua1901 <KaiserHua1901@outlook.com>
2024-01-01 13:47:40 -0800

    Add camera and Camera smoothing

---
+++ Scene/Camera/Camera2D.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+var target_position = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	#global_position = player.global_position
+	global_position = player.global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+	#Target_player_node()
+#func Target_player_node():
+	#var player_node = get_tree().get_nodes_in_group("player")
+	#if player_node.size() > 0:
+		#var player = player_node[0] as Node2D
+		#global_position = player.global_position
+
