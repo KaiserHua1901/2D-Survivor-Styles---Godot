7b099ba9b44b791872183cda4cb1a8f6c849e6ab
KaiserHua1901 <KaiserHua1901@outlook.com>
2024-01-02 14:21:17 -0800

    Add Enemy and pathfinding to player.

--- Scene/Camera/Camera2D.gd
+++ Scene/Camera/Camera2D.gd
@@ -3,15 +3,16 @@ var target_position = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	make_current()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	#global_position = player.global_position
-	global_position = player.global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+	global_position = player.global_position.lerp(target_position, 1.0 - exp(-delta * 15))
+	print(global_position)
 	#Target_player_node()
+	#global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 10))
 #func Target_player_node():
 	#var player_node = get_tree().get_nodes_in_group("player")
 	#if player_node.size() > 0:
