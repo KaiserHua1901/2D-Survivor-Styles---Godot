1722c48fb7ec311bffff36a7bb4af398e50089b9
KaiserHua1901 <KaiserHua1901@outlook.com>
2024-01-04 11:42:50 -0800

    Target enemies with sword ability

--- Scene/Ability/Sword_ability_controller/sword_ability_controller.gd
+++ Scene/Ability/Sword_ability_controller/sword_ability_controller.gd
@@ -1,5 +1,6 @@
 extends Node
 @export var Sword_Ability: PackedScene
+const MAX_RANGE = 200
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,10 +11,24 @@ func _ready():
 func On_Timer_timeout():
 	var player = get_tree().get_first_node_in_group('player') as Node2D
 	if player == null:
-		return
-	var sword_instantiate = Sword_Ability.instantiate() as Node2D
-	player.get_parent().add_child(sword_instantiate)
-	# add sword_ability as a instance and add as a child of main node
+		return 
 	
-	sword_instantiate.global_position = player.global_position
-	# this will appear in player postion when timer ran out 
+	var enemies = get_tree().get_nodes_in_group('enemy') 
+	enemies = enemies.filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2) 
+)
+# this should make the sword spawn when the enemy is around certain distance from player
+
+	enemies.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+	# this sort out first enemy in the array
+	if enemies.size() == 0:
+		return
+	
+	var sword_instance = Sword_Ability.instantiate() as Node2D
+	player.get_parent().add_child(sword_instance)
+	sword_instance.global_position = enemies[0].global_position
+	# this will spawn the sword as the first enemy after sorted
